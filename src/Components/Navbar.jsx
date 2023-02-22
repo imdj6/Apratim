d97fe31595ad0dd2c45bced1ba41b6c43750aa2d
sakshamaroraccet <sakshaminfinity9@gmail.com>
2023-02-22 20:49:14 +0530

    Modals are now working

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,11 +8,45 @@ import {
 import { GiHamburgerMenu } from "react-icons/gi";
 import Logo from "../assests/logo.png";
 import Button from "../UIComponents/Button";
+import Modal from "../UIComponents/Modal";
+import Login from "../UIComponents/Login";
+import Signup from "../UIComponents/signup";
 
 const Navbar = () => {
   const [showMediaIcons, setShowMediaIcons] = useState(false);
+  const [state, setState] = useState({
+    login: false,
+    signup: false,
+    forgot: false,
+  });
+  const changeLoginModalState = () => {
+    setState({
+      login: false,
+      signup: false,
+      forgot: false,
+    });
+  };
+  const changeSignupModalState = () => {
+    setState({
+      login: false,
+      signup: false,
+      forgot: false,
+    });
+  };
+  
   return (
     <>
+      {state.login && (
+        <Modal>
+          <Login changeLoginModalState={changeLoginModalState} />
+        </Modal>
+      )}
+      {state.signup && (
+        <Modal>
+          <Signup changeSignupModalState={changeSignupModalState} />
+        </Modal>
+      )}
+
       <nav className="main-nav">
         <div className="logo">
             <img className="h-24" src={Logo} alt="" />
@@ -29,8 +63,24 @@ const Navbar = () => {
 
         <div className="social-media">
           <ul className="social-media-desktop">
-            <li><Button buttonText="Register" active icon={AiOutlineUserAdd} /></li>
-            <li><Button buttonText="Login" icon={AiOutlineLogin} /></li>
+            <li><Button buttonText="Register" active icon={AiOutlineUserAdd}
+                  clickHandler={(e) => {
+                    setState({
+                      login: false,
+                      signup: true,
+                      forgot: false,
+                  }); }}/></li>
+            <li><Button
+              buttonText="Login"
+              icon={AiOutlineLogin}
+              clickHandler={(e) => {
+                setState({
+                  login: true,
+                  signup: false,
+                  forgot: false,
+                });
+              }}
+            /></li>
           </ul>
 
           {/* hamburget menu */}
